e925395ffaa90e797a24b7b61f99c5f80f45d3f8
semecan <semecan>
2020-08-19 16:57:03 +0200

    Add simple test for item sharing

--- Farmer_Connect/Tests/Send.lua
+++ Farmer_Connect/Tests/Send.lua
@@ -1,1 +1,20 @@
 local _, addon = ...;
+
+local detectors = addon.API.detectors;
+local GUILD_TYPE = addon.constants.MESSAGETYPES.guild;
+
+local function displayItem (sender, itemLink, count)
+  if (count > 1) then
+    print(sender, 'got', count, 'of', itemLink);
+  else
+    print(sender, 'got item:', itemLink);
+  end
+end
+
+addon.receive('test', function (sender, itemLink, count)
+  displayItem(sender, itemLink, tonumber(count));
+end);
+
+detectors.on('NEW_ITEM', function (itemInfo, count)
+  addon.send(GUILD_TYPE, 'test', itemInfo.link, count)
+end);
